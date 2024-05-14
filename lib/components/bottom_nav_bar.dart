d63c636b42539114a27963c12eab39fffa6348b7
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-14 14:13:05 +0900

    updated UI

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  final Function(int)? onTabChange;
+  const MyBottomNavBar({
+    super.key,
+    required this.onTabChange,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: GNav(
+        color: Colors.grey[400],
+        activeColor: const Color.fromARGB(255, 255, 136, 0),
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBorderRadius: 16,
+        onTabChange: (value) => onTabChange!(value),
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: 'Shop',
+          ),
+          GButton(
+            icon: Icons.shopping_bag_rounded,
+            text: 'Cart',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Image.asset('images/OKMPNG.png'),
+            ),
+            const SizedBox(height: 50),
+            const Text(
+              'Orange Kicks Manila',
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 136, 0),
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 5),
+            const Text(
+              'Your trusted one stop shop for affordable kicks.',
+              style: TextStyle(color: Colors.grey, fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 120),
+            GestureDetector(
+              onTap: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const HomePage())),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 228, 124, 6),
+                    borderRadius: BorderRadius.circular(12)),
+                child: const Padding(
+                  padding: EdgeInsets.all(25),
+                  child: Center(
+                    child: Text(
+                      'Shop Now',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
